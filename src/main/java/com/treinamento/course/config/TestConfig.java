f89eb72b16e7fa8427530b07fe4809099220092c
Joelcio Facanha <joelciomoreira@gmail.com>
2021-03-13 18:43:17 -0300

    Payment, one-to-one association

--- src/main/java/com/treinamento/course/config/TestConfig.java
+++ src/main/java/com/treinamento/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.treinamento.course.entities.Category;
 import com.treinamento.course.entities.Order;
 import com.treinamento.course.entities.OrderItem;
+import com.treinamento.course.entities.Payment;
 import com.treinamento.course.entities.Product;
 import com.treinamento.course.entities.User;
 import com.treinamento.course.entities.enums.OrderStatus;
@@ -77,6 +78,10 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 
 }
